fc01ef0d12ace797ab5adcc0962aea7744c51c7f
adrian.luethi <adrian.luethi@cirrus-i7.fritz.box>
2014-12-10 12:51:43 +0100

    organize

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
         <link rel="stylesheet" href="css/initialize.css">
         <link rel="stylesheet" href="css/loooping.css">
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-
     </head>
     <body>
         <!--[if lt IE 9]>
